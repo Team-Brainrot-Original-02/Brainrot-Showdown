288e7715d3efb8647cf47d96f7284627562e3525
mjcdr <arctic.iguana.327@gmail.com>
2025-05-29 15:49:55 +0200

    updated HomingMissile.cs to inflict damage

--- Assets/Code/Scripts_Marco/HomingMissle.cs
+++ Assets/Code/Scripts_Marco/HomingMissle.cs
@@ -5,6 +5,7 @@ public class HomingMissle : MonoBehaviour
     [SerializeField] private GameObject target;
     [SerializeField] private float speed = 3f;
     [SerializeField] private float distanceToTarget = 0.5f;
+    public float damage = 1f;
 
     private void Update()
     {
@@ -12,6 +13,18 @@ public class HomingMissle : MonoBehaviour
         {
             this.transform.position += (target.transform.position - this.transform.position).normalized * speed * Time.deltaTime;
             this.transform.LookAt(target.transform);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == target)
+        {
+            Health health = GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(damage);
+            }
+            Destroy(gameObject);
         }
     }
 }
